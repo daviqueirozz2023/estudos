81849c91ac27d83e1ca02fce6b8520a62208da5d
daviqueirozz2023 <daviqueirozzgithub@gmail.com>
2023-07-19 16:51:09 -0300

    inserido variavel const para manipular os arrays dos movimentos da cobra

--- Jogo da Cobrinha JS/js/script.js
+++ Jogo da Cobrinha JS/js/script.js
@@ -8,15 +8,63 @@ const size = 30
 
 const snake = [
     {x: 200, y: 200},
-    {x: 230, y: 200},
-    {x: 260, y: 200}
+    {x: 230, y: 200}
 ]
+
+let direction, loopid = ""
 
 const drawSnake = () => {
     ctx.fillStyle = "#ddd"
     //ctx. fillRect(snake[0].x, snake[0].y, size, size)
 
-    snake
+    snake.forEach((position, index) => {
+
+        if (index == snake.length -1){
+            ctx.fillStyle = "#fff"
+        }
+
+        ctx.fillRect(position.x, position.y, size, size)
+
+
+    })
 }
 
-drawSnake()
+const moveSnake = () => {
+    if(!direction) return
+    
+    const head = snake[snake.length -1]
+
+    
+
+    if(direction == "right") {
+        snake.push({ x: head.x + size, y: head.y })
+
+    }
+
+    if(direction == "left"){
+        snake.push({x: head.x - size, y: head.y})
+    }
+
+    if(direction == "down") {
+        snake.push({x: head.x, y: head.y + size})
+    }
+
+    if(direction == "up"){
+        snake.push({x: head.x, y: head.y - size})
+    }
+
+    snake.shift()
+}
+
+const gameLoop = () => {
+    clearInterval(loopid)
+    ctx.clearRect(0, 0, 600, 600)
+    moveSnake()
+    drawSnake()
+
+    loopid = setTimeout(() => {
+        gameLoop()
+    },300)
+}
+
+gameLoop()
