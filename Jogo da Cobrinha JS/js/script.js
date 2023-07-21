581a0d5f2d31c204241eeb9f4a0c88c48fb780c0
daviqueirozz2023 <daviqueirozzgithub@gmail.com>
2023-07-21 15:36:56 -0300

    Continuando o desenvolvimento do jogo da cobrinha inserindo randomNumber para a comida cair aleatóriamente no mapa

--- Jogo da Cobrinha JS/js/script.js
+++ Jogo da Cobrinha JS/js/script.js
@@ -1,5 +1,7 @@
 const canvas = document.querySelector("canvas")
 const ctx = canvas.getContext("2d")
+
+const h1 = document.querySelector("h1")
 //esplicando a cor do quadrado e as coordenadas
 //ctx.fillStyle = "blue"
 //ctx.fillRect(300, 300, 50, 50)
@@ -7,11 +9,34 @@ const ctx = canvas.getContext("2d")
 const size = 30
 
 const snake = [
-    {x: 200, y: 200},
-    {x: 230, y: 200}
+    {x: 270, y: 240}
 ]
 
-let direction, loopid = ""
+const randomNumber = (min, max) => {
+    // Calculo para retonar numero inteiro acima de 5
+    //return Math.round(Math.random() * (10 - 5) + 5)
+    return Math.round(Math.random() * (max - min) + min)
+}
+
+h1.innerText = randomNumber(5, 10)
+
+const food = {
+    x: randomNumber(0, 570),
+    y: randomNumber(0, 570),
+    color: "yellow"
+}
+
+let direction, loopid
+
+const drawFood = () => {
+    const {x, y, color} = food
+
+    ctx.shadowColor = color
+    ctx.shadowBlur = 10
+    ctx.fillStyle = color
+    ctx.fillRect(food.x, food.y, size, size)
+    ctx.shadowBlur = 0
+}
 
 const drawSnake = () => {
     ctx.fillStyle = "#ddd"
@@ -56,9 +81,31 @@ const moveSnake = () => {
     snake.shift()
 }
 
+const drawGrid = () => {
+    ctx.lineWidth = 1
+    ctx.strokeStyle = "#333"
+
+    for (let i = 30; i < canvas.width; i += 30){
+
+        ctx.beginPath()
+        ctx.lineTo(i, 0)
+        ctx.lineTo(i, 600)
+        ctx.stroke()
+
+        ctx.beginPath()
+        ctx.lineTo(0, i)
+        ctx.lineTo(600, i)
+        ctx.stroke()
+
+    }
+}
+drawGrid()
+
 const gameLoop = () => {
     clearInterval(loopid)
     ctx.clearRect(0, 0, 600, 600)
+    drawGrid()
+    drawFood()
     moveSnake()
     drawSnake()
 
@@ -68,3 +115,23 @@ const gameLoop = () => {
 }
 
 gameLoop()
+
+document.addEventListener("keydown", ({key}) => {
+
+    if (key == "ArrowRight" && direction != "left"){
+        direction = "right"
+    }
+
+    if(key == "ArrowLeft" && direction != "right"){
+        direction = "left"
+    }
+
+    if(key == "ArrowDown" && direction != "up"){
+        direction = "down"
+    }
+
+    if(key == "ArrowUp" && direction != "down"){
+        direction = "up"
+    }
+   
+})
