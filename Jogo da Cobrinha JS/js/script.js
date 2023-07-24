917920ef1e2302e43773cbfa45c11b5ad8fb20cd
daviqueirozz2023 <daviqueirozzgithub@gmail.com>
2023-07-24 13:51:54 -0300

    Continuando o desenvolvimento do jogo da cobrinha inserindo a variável gameOver se a cobrinha bate na parede ou ela come o próprio rabo

--- Jogo da Cobrinha JS/js/script.js
+++ Jogo da Cobrinha JS/js/script.js
@@ -1,7 +1,14 @@
 const canvas = document.querySelector("canvas")
 const ctx = canvas.getContext("2d")
 
-const h1 = document.querySelector("h1")
+const score = document.querySelector(".score--value")
+const finalScore = document.querySelector(".final-score > span")
+const menu = document.querySelector(".menu-screen")
+const buttonPlay = document.querySelector(".btn-play")
+
+const audio = new Audio ("/Jogo da Cobrinha JS/js/audio.mp3")
+
+//const h1 = document.querySelector("h1")
 //esplicando a cor do quadrado e as coordenadas
 //ctx.fillStyle = "blue"
 //ctx.fillRect(300, 300, 50, 50)
@@ -12,18 +19,36 @@ const snake = [
     {x: 270, y: 240}
 ]
 
+const incrementScore = () => {
+    //score.innerText = + score.innerText + 10 // Também funciona
+    score.innerText = parseInt(score.innerText) + 10
+}
+
 const randomNumber = (min, max) => {
     // Calculo para retonar numero inteiro acima de 5
     //return Math.round(Math.random() * (10 - 5) + 5)
     return Math.round(Math.random() * (max - min) + min)
 }
 
-h1.innerText = randomNumber(5, 10)
+const randomPosition = () => {
+    const number = randomNumber(0, canvas.width - size)
+    return Math.round(number / 30) * 30
+}
+
+const randomColor = () => {
+    const red = randomNumber(0, 255)
+    const green = randomNumber(0, 255)
+    const blue = randomNumber(0, 255)
+
+    return `rgb(${red}, ${green}, ${blue})`
+}
+
+//h1.innerText = randomColor()
 
 const food = {
-    x: randomNumber(0, 570),
-    y: randomNumber(0, 570),
-    color: "yellow"
+    x: randomPosition(),
+    y: randomPosition(),
+    color: randomColor()
 }
 
 let direction, loopid
@@ -99,7 +124,53 @@ const drawGrid = () => {
 
     }
 }
-drawGrid()
+//drawGrid()
+
+const checkEAT = () => {
+    const head = snake[snake.length - 1]
+
+    if (head.x == food.x && head.y == food.y){
+        incrementScore()
+        snake.push(head)
+        audio.play()
+
+        let x = randomPosition()
+        let y = randomPosition()
+        while (snake.find((position) => position.x && position.y == y )){
+             x = randomPosition()
+             y = randomPosition()
+        }
+        food.x = x
+        food.y = y
+        food.color = randomColor()
+    }
+}
+
+const checkCollision = () => {
+    const head = snake[snake.length - 1]
+    const canvasLimit = canvas.width - size
+    const neckIndex = snake.length - 2
+
+    const wallCollision = 
+    head.x < 0 || head.x > canvasLimit || head.y < 0 || head.y > canvasLimit
+
+    const selfCollision = snake.find((position, index) => {
+        return index < neckIndex && position.x == head.x && position.y == head.y
+
+    })
+
+    if (wallCollision || selfCollision){
+        gameOver()
+    } 
+}
+
+const gameOver = () =>{
+
+    direction = undefined
+
+    menu.style.display = "flex"
+    finalScore.innerText - score
+}
 
 const gameLoop = () => {
     clearInterval(loopid)
@@ -108,6 +179,8 @@ const gameLoop = () => {
     drawFood()
     moveSnake()
     drawSnake()
+    checkEAT()
+    checkCollision()
 
     loopid = setTimeout(() => {
         gameLoop()
